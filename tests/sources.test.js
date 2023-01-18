5b072a5c24bcfe30ffc53d6f37b99aa4abeaf7ce
Antonis Mavr <antonisira@gmail.com>
2023-01-18 19:11:10 +0200

    Update sources.test.js

--- tests/sources.test.js
+++ tests/sources.test.js
@@ -26,7 +26,7 @@ test.after.always((t) => {
   t.context.server.close();
 });
 
-console.log(token);
+const authtoken = process.env.AUTHTOKEN2;
 const test_user = {
   "username": "usertesting",
   "id": "63c7aa08b0ae7a0550648094",
@@ -35,39 +35,129 @@ const test_user = {
 
 // Test for post req sources/sources
 test('GET /sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
+  const token = authtoken;
   const {body,statusCode} = await t.context.got(`sources/sources?token=${token}`);
   t.is(statusCode, 200);
   t.assert(body.success);
 });
 
 
-/*
 // Test for post req sources/create-source
-test('GET /create-sources returns correct response and status code', async (t) => {
-  const {body,statusCode} = await t.context.got(`sources/create-source?token=${token}`);
-  t.assert(body.success);
+test('POST /create-sources returns correct response and status code', async (t) => {
+  const token = authToken2;
+  const id = '3'
+  const name = 'source3';
+  const type = 'type3';
+  const login = 'login3';
+  const url = 'url3';
+  const vhost = 'vhost3';
+  const passcode = 'pass3';
+
+  const body = await t.context.got.post(`sources/create-source?token=${token}&id=${id}`, {
+    json: { name, type, url, login, passcode, vhost }
+  }).json();
+
+  if (body.sucess) {
+    t.assert(body.success);
+  }
+  else {
+    t.is(body.status, 409);
+  }
 });
 
 // Test for post req sources/change-source
 test('GET /change-sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
-  const {body,statusCode} = await t.context.got(`sources/change-source?token=${token}`);
-  console.log(body);
-  t.assert(body.success);
+  const token = authtoken;
+  const id = '63c8127187ee7205495aa3bd' 
+  const name = 'source3';
+  const type = 'new_type';
+  const url = 'new_url';
+  const login = 'new_login';
+  const passcode = 'new_pass';
+  const vhost = 'new_vhost';
+
+  const body = await t.context.got.post(`sources/change-source?token=${token}&id=${id}`, {
+    json: { id, name, type, url, login, passcode, vhost }
+  }).json();
+
+  if (body.success) {
+    t.assert(body.success);
+  }
+  else {
+    t.is(body.status, 409)
+  }
 });
 
-// Test for post req sources/delete-source
+// Test for post req sources/change-source with wrong id
+test('GET /change-sources returns correct response and status code', async (t) => {
+  const token = authtoken;
+  const id = '63c8127187ee7205495ad3bc' 
+  const name = 'source3';
+  const type = 'new_type';
+  const url = 'new_url';
+  const login = 'new_login';
+  const passcode = 'new_pass';
+  const vhost = 'new_vhost';
+
+  const body = await t.context.got.post(`sources/change-source?token=${token}&id=${id}`, {
+    json: { id, name, type, url, login, passcode, vhost }
+  }).json();
+
+  if (body.success) {
+    t.assert(body.success);
+  }
+  else {
+    t.is(body.status, 409)
+  }
+});
+
+// Test for post req sources/delete-source with worng id
 test('GET /delete-sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
-  const {body,statusCode} = await t.context.got(`sources/delete-source?token=${token}`);
-  t.assert(body.success);
+  const token = authtoken;
+  const id = '63c8127187ee7205495ad3bc' 
+  const {body,statusCode} = await t.context.got(`sources/delete-source?token=${token}`,{
+    json: { id }
+  }).json();
+    t.is(body.status, 409);
 });
 
 // Test for post req sources/check-sources
 test('GET /check-sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
-  const {body,statusCode} = await t.context.got(`sources/check-sources?token=${token}`);
+
+  const token = authtoken;
+  const id = '63c8127187ee7205495ad3bc' 
+  const sources = ['another_source', 's2', 's3']; 
+  const body = await t.context.got.post(`sources/check-sources?token=${token}&id=${id}`, {
+    json: { sources }
+  }).json();
+
   t.assert(body.success);
 });
-*/
+
+// Test for post /source with correct name
+test('POST /source returns correct response', async (t) => {
+
+  const name = 'source'; //wrong source name
+  const owner = 'self';
+  const user = testing_user;
+
+  const body = await t.context.got.post(`sources/source`, {
+    json: { name, owner, user }
+  }).json();
+
+  t.assert(body.success);
+});
+
+// Test for post /source with wrong name
+test('POST /source returns 409 if source not found', async (t) => {
+
+  const name = 's5'; 
+  const owner = 'self';
+  const user = testing_user;
+
+  const body = await t.context.got.post(`sources/source`, {
+    json: { name, owner, user }
+  }).json();
+
+  t.is(body.status, 409);
+});
