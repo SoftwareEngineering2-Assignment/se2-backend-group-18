5b050d56c46e1068039932d2a797c73352a7689b
Thomas Pliakis <tpliakis@ece.auth.gr>
2023-01-18 11:23:59 +0200

    fail

--- tests/sources.test.js
+++ tests/sources.test.js
@@ -12,6 +12,7 @@ const {jwtSign} = require('../src/utilities/authentication/helpers');
 require('dotenv').config(app.env);
 //console.log(process.env);
 
+
 // This runs before everything else
 // t is an object from ava
 test.before(async (t) => {
@@ -25,38 +26,48 @@ test.after.always((t) => {
   t.context.server.close();
 });
 
+console.log(token);
+const test_user = {
+  "username": "usertesting",
+  "id": "63c7aa08b0ae7a0550648094",
+  "email": "usertesting@gmail.com"
+};
+
 // Test for post req sources/sources
 test('GET /sources returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
+  const {body,statusCode} = await t.context.got(`sources/sources?token=${token}`);
   t.is(statusCode, 200);
+  t.assert(body.success);
 });
 
 
+/*
 // Test for post req sources/create-source
 test('GET /create-sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/create-source?token=${token}`);
-  t.assert(success);
+  const {body,statusCode} = await t.context.got(`sources/create-source?token=${token}`);
+  t.assert(body.success);
 });
 
 // Test for post req sources/change-source
 test('GET /change-sources returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/change-source?token=${token}`);
-  t.assert(success);
+  const {body,statusCode} = await t.context.got(`sources/change-source?token=${token}`);
+  console.log(body);
+  t.assert(body.success);
 });
 
 // Test for post req sources/delete-source
 test('GET /delete-sources returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/delete-source?token=${token}`);
-  t.assert(success);
+  const {body,statusCode} = await t.context.got(`sources/delete-source?token=${token}`);
+  t.assert(body.success);
 });
 
 // Test for post req sources/check-sources
 test('GET /check-sources returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/check-sources?token=${token}`);
-  t.assert(success);
+  const {body,statusCode} = await t.context.got(`sources/check-sources?token=${token}`);
+  t.assert(body.success);
 });
+*/
