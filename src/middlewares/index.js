7630e18d16e9c7b70d5ca180fd470a4f388a288c
Antonis Mavr <antonisira@gmail.com>
2023-01-20 15:11:14 +0200

    Update index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,7 @@
 const authorization = require('./authorization');
 const error = require('./error');
 const validation = require('./validation');
-
+/* main exposrt */
 module.exports = {
   authorization,
   error,
