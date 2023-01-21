2651866eb1a3320f8215ee52878305f02f06a74a
Antonis Mavr <antonisira@gmail.com>
2023-01-21 23:21:34 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,11 @@
+/*
+This script creates an Express.js Router that defines routes for different functionality
+in the application. It exports the router, which will be used in the main application.
+It uses 5 other routes files: users, sources, dashboards, general and root. It maps the
+routes to the corresponding router. It makes sure that different requests to the server
+are handled by the appropriate router based on the request path.
+*/
+
 const express = require('express');
 const users = require('./users');
 const sources = require('./sources');
