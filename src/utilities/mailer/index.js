c75dda03f0392792fe1a9c393529b1df19283cf1
Antonis Mavr <antonisira@gmail.com>
2023-01-21 22:39:01 +0200

    Update index.js

--- src/utilities/mailer/index.js
+++ src/utilities/mailer/index.js
@@ -1,3 +1,8 @@
+/* 
+This script exports an object that contains two properties which are the exports
+of two other modules, password and send.
+*/
+
 const password = require('./password');
 const send = require('./send');
 
