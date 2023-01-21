733f9c4950b2312cfb560c0b6786d8bf5217cf9c
Antonis Mavr <antonisira@gmail.com>
2023-01-21 22:19:41 +0200

    Update index.js

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,8 @@
+/*
+This script exports an object that contains a single property, mailer, which is the result of requiring the ./mailer module.
+This means that the exported object has a reference to the exports of the ./mailer module, and therefore any methods or 
+properties that are exported by the ./mailer module will be available on the mailer property of the object exported by this script.
+*/
 const mailer = require('./mailer');
 
 module.exports = {
