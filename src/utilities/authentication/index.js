a9fa717826c220ad2f5d101b4f9cbcd11f106ec4
Antonis Mavr <antonisira@gmail.com>
2023-01-21 22:52:58 +0200

    Update index.js

--- src/utilities/authentication/index.js
+++ src/utilities/authentication/index.js
@@ -1,3 +1,6 @@
+/* This script exports an object with a property 'helpers' that references the
+exports of a file named 'helpers' located in the same directory. */
+
 const helpers = require('./helpers');
 
 module.exports = {helpers};
